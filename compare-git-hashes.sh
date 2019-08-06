0e2fe7570ec2211611905c96423af80663236db5
Nicusor Serban <serban.nicusor@toptal.com>
2019-08-06 21:26:33 +0200

    Pushing fix

--- compare-git-hashes.sh
+++ compare-git-hashes.sh
@@ -77,11 +77,11 @@ set -e -x
 # First checkout the first arg cmdstan hash, assuming stan and math are as specified
 # by that cmdstan commit
 clean_checkout "$2" "false" "false"
-./runPerformanceTests.py --overwrite-golds $1
-
-for i in performance.*; do
-    mv $i "${2}_${i}"
-done
+NAME1="reference-`date "+%y-%h-%m-%s"`"
+./runPerformanceTests.py --overwrite-golds $1 --name="$NAME1"
 
 clean_checkout "$3" "$4" "$5"
-./runPerformanceTests.py --check-golds-exact 1e-8 $1 && ./comparePerformance.py "${2}_performance.csv" performance.csv
+NAME2="performance"
+./runPerformanceTests.py --check-golds-exact 1e-8 $1
+
+./comparePerformance.py "$NAME1.csv" "$NAME2.csv"
