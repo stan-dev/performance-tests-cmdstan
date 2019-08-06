f9ea5e6f82c3cb57d34a2bbeeccf1a54345a4eee
Nicusor Serban <serban.nicusor@toptal.com>
2019-08-06 20:08:16 +0200

    Modified compare git hashes to reflect compare compilers.

--- compare-git-hashes.sh
+++ compare-git-hashes.sh
@@ -79,10 +79,12 @@ clean_checkout "$2" "false" "false"
 NAME1="reference-`date "+%y-%h-%m-%s"`"
 ./runPerformanceTests.py --overwrite-golds $1 --name="$NAME1"
 
-for i in performance.*; do
-    mv $i "${2}_${i}"
-done
+#for i in performance.*; do
+#    mv $i "${2}_${i}"
+#done
 
 clean_checkout "$3" "$4" "$5"
 NAME2="performance"
-./runPerformanceTests.py --check-golds-exact 2e-8 $1 && ./comparePerformance.py "${2}_performance.csv" performance.csv --name="$NAME2"
+./runPerformanceTests.py --check-golds-exact 2e-8 $1 --name="$NAME2"
+
+./comparePerformance.py "$NAME1.csv" "$NAME2.csv"
