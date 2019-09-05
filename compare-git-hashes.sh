3fba94e06ea81127e3fea2ba83963ae6a3ceede9
Sean Talts <sean.talts@gmail.com>
2019-09-05 17:01:31 -0400

    Revert to unnamed perf results for comparison scripts

--- compare-git-hashes.sh
+++ compare-git-hashes.sh
@@ -76,12 +76,11 @@ set -e -x
 # First checkout the first arg cmdstan hash, assuming stan and math are as specified
 # by that cmdstan commit
 clean_checkout "$2" "false" "false"
-NAME1="reference-`date "+%y-%h-%m-%s"`"
-./runPerformanceTests.py --overwrite-golds $1 --name="$NAME1"
+./runPerformanceTests.py --overwrite-golds $1
+
+for i in performance.*; do
+    mv $i "${2}_${i}"
+done
 
 clean_checkout "$3" "$4" "$5"
-NAME2="performance"
-./runPerformanceTests.py --check-golds-exact 1e-8 $1 --name="$NAME2"
-
-./comparePerformance.py "$NAME1.csv" "$NAME2.csv"
-
+./runPerformanceTests.py --check-golds-exact 2e-8 $1 && ./comparePerformance.py "${2}_performance.csv" performance.csv
