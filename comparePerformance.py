1f0914313f43909035d37d6f60e81e0c3f0ceda4
Nicusor Serban <serban.nicusor@toptal.com>
2019-10-27 00:45:10 +0200

    Reverted output changes

--- comparePerformance.py
+++ comparePerformance.py
@@ -26,9 +26,8 @@ if __name__ == "__main__":
 
     f = open(os + "_compare_results.xml", "w+")
 
-    #<testcase name="compilation" result="99.7739980221"/>
     for r in ratios:
-        f.write("<testcase name=\"" + str(r[0]) + "\" time=\"" + str(round(r[1], 2)) + "\"/>")
+        f.write(str(r[0]) + ", " + str(round(r[1], 2)))
 
     f.write(str(mean([r for _, r in ratios])))
 
