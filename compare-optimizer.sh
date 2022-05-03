4abbe8a77a5f3f95f5091b5b4dd42be66edf22a4
Rok Češnovar <rok.cesnovar@fri.uni-lj.si>
2022-05-03 16:16:20 +0200

    Update compare-optimizer.sh

--- compare-optimizer.sh
+++ compare-optimizer.sh
@@ -16,8 +16,8 @@ set -e -x
 # run with the default optimization level
 cd cmdstan; make clean-all; make -j4 build;
 if [ -n "$3" ] ; then
-    rm cmdstan/bin/stanc
-    cp "$3" cmdstan/bin/stanc
+    rm bin/stanc
+    cp "$3" bin/stanc
 fi
 make -j4 examples/bernoulli/bernoulli; ./bin/stanc --version; cd ..
 ./runPerformanceTests.py --overwrite-golds $1
