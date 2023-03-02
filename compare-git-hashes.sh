48a459fd37ba51d01fac5fb7aec623c3108fb148
Nicusor Serban <nserban@dymeng.com>
2023-03-02 20:42:04 +0100

    Make sure to run performance tests with custom stanc3 bin url

--- compare-git-hashes.sh
+++ compare-git-hashes.sh
@@ -7,7 +7,7 @@ usage() {
 }
 
 write_makelocal() {
-    echo "CXXFLAGS += -march=native \n${4}" > make/local
+    echo "CXXFLAGS += -march=native \n${1}" > make/local
 }
 
 clean_checkout() {
@@ -61,7 +61,12 @@ clean_checkout() {
     #    echo "ERROR: Git repo isn't clean - I'd recommend you make a separate recursive clone of CmdStan for this."
     #    exit
     #fi
-    write_makelocal
+    # If we're not checking develop for cmdstan, use custom stanc3 binary url
+    if [[ "$1" != "develop" ]] ; then
+      write_makelocal "$6"
+    else
+      write_makelocal ""
+    fi
     git status
     cd ..
 }
@@ -73,9 +78,7 @@ fi
 
 set -e -x
 
-# First checkout the first arg cmdstan hash, assuming stan and math are as specified
-# by that cmdstan commit
-
+# Checkout base cmdstan
 clean_checkout "$2" "false" "false"
 ./runPerformanceTests.py --overwrite-golds $1
 
@@ -83,5 +86,6 @@ for i in performance.*; do
     mv $i "${2}_${i}"
 done
 
+# Checkout cmdstan PR ( will use
 clean_checkout "$3" "$4" "$5"
 ./runPerformanceTests.py --check-golds $1 && ./comparePerformance.py "${2}_performance.csv" performance.csv markdown
